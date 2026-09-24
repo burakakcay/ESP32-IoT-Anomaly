fee311be0aaf69829a26d4499cc1796941720139
Burak Akçay <burakakcay013@gmail.com>
2026-09-24 16:18:35 +0300

    admin paneli hazırlıkları

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 
 function createApp() {
   const app = express();
 
-  app.use(express.json());
+  app.use(
+    cors({
+      origin: "http://localhost:5000",
+      methods: ["GET"],
+      allowedHeaders: ["Authorization", "Content-Type"],
+    }),
+  );
+
+  app.use(express.json);
 
   return app;
 }
